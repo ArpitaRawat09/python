575d91ab7fe0531d08b3f9dc76aa01e8a2f1158b
Arpita Rawat <arpitarawat381@gmail.com>
2026-08-02 22:25:12 +0530

    control flow

--- basicPracticeQ.py
+++ basicPracticeQ.py
@@ -1,8 +1,38 @@
 # Answer True or False
 
-# Assignment Questions 
+# Assignment Questions
 
-print(126 < 120)
-print((456 == 456) != (235 == 236))
-print(12 < 10 or 45 == 56 or 69 > 70 or 15 != 13)
-print(True and bool(0))
+# print(126 < 120)
+# print((456 == 456) != (235 == 236))
+# print(12 < 10 or 45 == 56 or 69 > 70 or 15 != 13)
+# print(True and bool(0))
+
+
+# Conditional Statements Questions
+
+# a = 13
+# if a>10:
+#     print("I will do task A...")
+# else:
+#     print("I will do task B...")
+
+
+# a = 5
+# if a > 18 and a < 40:
+#     print("You are Adult")
+# elif a < 18:
+#     print("You are minor")
+# else:
+#     print("You are older")
+
+
+# Assignment 3
+# Question 1
+
+num1 = int(input("Enter first number :- "))
+num2 = int(input("Enter second number :- "))
+
+if num1>num2:
+    print("num1 is greater than num2")
+else:
+    print("num2 is greater than num1")
