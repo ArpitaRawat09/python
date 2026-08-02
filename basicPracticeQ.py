ec197a800c829a059ff6383600538537204ac309
Arpita Rawat <arpitarawat381@gmail.com>
2026-08-02 17:45:29 +0530

    Assignment 2 Questions

---
+++ basicPracticeQ.py
@@ -0,0 +1,8 @@
+# Answer True or False
+
+# Assignment Questions 
+
+print(126 < 120)
+print((456 == 456) != (235 == 236))
+print(12 < 10 or 45 == 56 or 69 > 70 or 15 != 13)
+print(True and bool(0))
