3c98502ba6195eba752795127a781eb98e7ff10e
Arpita Rawat <arpitarawat381@gmail.com>
2026-08-06 12:37:09 +0530

    Use of Loops in String

--- loops.py
+++ loops.py
@@ -1,20 +1,27 @@
-# Basic Examples for practice
+# Basic Examples for Loops practice
 
-for i in range(1, 21):
-    print(i)
-
-
-# Reverse loop
-for i in range(20, 0, -1):
-    print(i)
+# for i in range(1, 21):
+#     print(i)
 
 
-# table
-num = 2
-for i in range(1, 11):
-    print(f"{num} * {i} = {num*i}")
+# # Reverse loop
+# for i in range(20, 0, -1):
+#     print(i)
 
-n = int(input("which table you want to print :- "))
-for i in range(n, n * 10 + 1, n):
-    # print(f"{n} * {i} = {n*i}")
-    print(i)
+
+# # table
+# num = 2
+# for i in range(1, 11):
+#     print(f"{num} * {i} = {num*i}")
+
+# n = int(input("which table you want to print :- "))
+# for i in range(n, n * 10 + 1, n):
+#     # print(f"{n} * {i} = {n*i}")
+#     print(i)
+
+# Looops for String
+
+a = "I am a full stack web Developer"
+print(len(a))
+# for i in range(len(a)):
+#     print(a[i])
