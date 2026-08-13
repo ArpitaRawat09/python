f0f0f2c330798e3f961c1443a9da0a1c00fb8e0f
Arpita Rawat <arpitarawat381@gmail.com>
2026-08-13 23:12:13 +0530

    palindrome Question

--- string.py
+++ string.py
@@ -1,6 +1,22 @@
-name = "ArpitaRawatfromBagdakhurd"
-store = ""
+# Palindrome Question
+
+name = "nama"
+rev = ""
+cpy = name
 for i in range(len(name) - 1, -1, -1):
-    store = store + name[i]
-print(store)
-    # print(name[i])
+    rev = rev + name[i]
+if cpy == rev:
+    print("String is Palindrome")
+else:
+    print("String is not Palindrome")
+
+
+
+
+
+# name = "ArpitaRawatfromBagdakhurd"
+# store = ""
+# for i in range(len(name) - 1, -1, -1):
+#     store = store + name[i]
+# print(store)
+#     # print(name[i])
