c8bf3323b47dd08e304c0113d0247298d35fc343
Arpita Rawat <arpitarawat381@gmail.com>
2026-08-13 23:36:39 +0530

    counting question

--- string.py
+++ string.py
@@ -1,16 +1,30 @@
+# Count special characters
+str = "a123evnu#%1!*najia"
+
+digit = 0
+char = 0
+schar = 0
+for i in str:
+    if i.isdigit():
+        digit += 1
+    elif i.isalpha():
+        char += 1
+    else:
+        schar += 1
+
+print(f"Digits are = {digit} \nCharacters are = {char} \nSpecial char are = {schar}")
+
+
 # Palindrome Question
 
-name = "naman"
-rev = ""
-for i in range(len(name) - 1, -1, -1):
-    rev = rev + name[i]
-if name == rev:
-    print("String is Palindrome")
-else:
-    print("String is not Palindrome")
-
-
-
+# name = "naman"
+# rev = ""
+# for i in range(len(name) - 1, -1, -1):
+#     rev = rev + name[i]
+# if name == rev:
+#     print("String is Palindrome")
+# else:
+#     print("String is not Palindrome")
 
 
 # name = "ArpitaRawatfromBagdakhurd"
