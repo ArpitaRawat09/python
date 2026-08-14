c31510beb60ec33532e388c44b4f3aeef42d8c61
Arpita Rawat <arpitarawat381@gmail.com>
2026-08-14 10:32:57 +0530

    types of functions

--- functions.py
+++ functions.py
@@ -1,6 +1,30 @@
+# position function
+def sum(a, b):
+    print(f"Sum of a and b is = {a+b}")
+
+# sum(76, 89)
+
+
+# Default function
+def greet(name="Arpita"):
+    print(f"Namsate {name}")
+
+
+# greet()
+# greet("Bhaven")
+
+
+# keyword function
+def detail(name, age):
+    print(f"Name is {name} and age is {age}")
+
+
+detail(age=22, name="Arpita")
+
+
 # function syntax
 
-def hello():
-    print("hello I am a python Function......!")
+# def hello():
+#     print("hello I am a python Function......!")
 
-hello()    
+# hello()
