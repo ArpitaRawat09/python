25498554cd423a7af53b885acbff1fc6edb8c294
Arpita Rawat <arpitarawat381@gmail.com>
2026-08-14 12:47:21 +0530

    list question

--- DS/list.py
+++ DS/list.py
@@ -1,27 +1,56 @@
-# create a list
+# Questions
 
-# x = [1,2,3,4,5]
-# y = [1, "Arpita", 22.5, True]
-# # y.append(11)
-# x = y.copy()
-# # y.clear()
+# Print +ve and -ve number in the list
 
-# # print(type(y))
-# print(x)
+num = [10,-23,43,76,-90,-2,-34]
+count = 0
+
+for i in num:
+    if i <= 0:
+        count = count + 1
+        # print(i ," is negative")
+    # else:
+        # print(i," is positive")  
+
+        
+print(count)  
 
 
-fruits = ["Apple", "Banana", "Cherry", "Grapes"]
-vegis = ["laddiesFingure" , "tomato" , "patato" ,"Chilli"]
 
-# fruits.pop(1)
 
-# fruits.extend(vegis)
 
-# x = fruits.count("Cherry")
 
-fruits.reverse()
-vegis.sort()
 
+
+
+
+
+
+
+# # create a list
+
+# # x = [1,2,3,4,5]
+# # y = [1, "Arpita", 22.5, True]
+# # # y.append(11)
+# # x = y.copy()
+# # # y.clear()
+
+# # # print(type(y))
+# # print(x)
+
+
+# fruits = ["Apple", "Banana", "Cherry", "Grapes"]
+# vegis = ["laddiesFingure" , "tomato" , "patato" ,"Chilli"]
+
+# # fruits.pop(1)
+
+# # fruits.extend(vegis)
+
+# # x = fruits.count("Cherry")
+
+# fruits.reverse()
+# vegis.sort()
+
+# # print(fruits)
 # print(fruits)
-print(fruits)
-print(vegis)
+# print(vegis)
