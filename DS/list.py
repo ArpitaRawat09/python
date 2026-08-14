ec9a0fbb3dbb91d9a500fa420b344bfe4461ed2f
Arpita Rawat <arpitarawat381@gmail.com>
2026-08-14 11:54:28 +0530

    practice of list methods

--- DS/list.py
+++ DS/list.py
@@ -1,10 +1,27 @@
 # create a list
 
 # x = [1,2,3,4,5]
-y = [1, "Arpita", 22.5, True]
-# y.append(11)
-x = y.copy()
-# y.clear()
+# y = [1, "Arpita", 22.5, True]
+# # y.append(11)
+# x = y.copy()
+# # y.clear()
 
-# print(type(y))
-print(x)
+# # print(type(y))
+# print(x)
+
+
+fruits = ["Apple", "Banana", "Cherry", "Grapes"]
+vegis = ["laddiesFingure" , "tomato" , "patato" ,"Chilli"]
+
+# fruits.pop(1)
+
+# fruits.extend(vegis)
+
+# x = fruits.count("Cherry")
+
+fruits.reverse()
+vegis.sort()
+
+# print(fruits)
+print(fruits)
+print(vegis)
