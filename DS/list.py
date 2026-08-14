f00f52bd4643f65caae87eba419269670bcf0b3a
Arpita Rawat <arpitarawat381@gmail.com>
2026-08-14 11:45:36 +0530

    some list methods

--- DS/list.py
+++ DS/list.py
@@ -1,6 +1,10 @@
 # create a list
 
 # x = [1,2,3,4,5]
-y = [1,"Arpita",22.5,True]
+y = [1, "Arpita", 22.5, True]
+# y.append(11)
+x = y.copy()
+# y.clear()
 
-print(type(y))
+# print(type(y))
+print(x)
