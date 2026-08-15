8ce6ab94d2de7d32d40b9c235219e079198af78a
Arpita Rawat <arpitarawat381@gmail.com>
2026-08-15 23:44:53 +0530

    sets methods

--- DS/sets.py
+++ DS/sets.py
@@ -1,3 +1,32 @@
-thisset = {"apple", "banana", "cherry", True, 1, 2,8,"arpita" , False,0}
+# thisset = {"apple", "banana", "cherry", True, 1, 2,8,"arpita" , False,0}
 
-print(thisset)
+# print(thisset)
+
+
+# Methods of Sets
+
+fruits = {"apple", "banana", "cherry"}
+# fruits.add("orange")
+fruits.add("apple")
+
+print(fruits)
+
+
+
+fruits = {"apple", "banana", "cherry"}
+
+fruits.clear()
+
+print(fruits)
+
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+z = x.difference(y)
+x.difference_update(y)
+z = x.intersection(y)
+
+print(z)
+print(x)
+print(z)
