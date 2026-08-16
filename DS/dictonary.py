86d5d20961b32e72456da0d9b9ef30fb1c4cbb16
Arpita Rawat <arpitarawat381@gmail.com>
2026-08-16 12:48:38 +0530

    sum question

--- DS/dictonary.py
+++ DS/dictonary.py
@@ -1,15 +1,30 @@
+# sum question
+
+d1 = {1: 100, 2: 200, 3: 300, 4: 400}
+d2 = {4: 600, 5: 500, 6: 200}
+
+for i in d2:
+    if i in d1.keys():
+        d1[i] += d2[i]
+
+    else:
+        d1[i] = d2[i]
+
+print(d1)
+
+
 # Find the frequency
 
-x = [1, 1, 2, 3, 2, 4, 3, 5, 9, 9, 9, 2, 4, 1, 1, 0]
-d = {}
-freq = 0
-for i in x:
-    if i in d.keys():
-        d[i] += 1
-    else:
-        d[i] = 1    
+# x = [1, 1, 2, 3, 2, 4, 3, 5, 9, 9, 9, 2, 4, 1, 1, 0]
+# d = {}
+# freq = 0
+# for i in x:
+#     if i in d.keys():
+#         d[i] += 1
+#     else:
+#         d[i] = 1
 
-print(d)
+# print(d)
 
 
 # dictonary = {"name": "Arpita", "age ": 22, "degree": "MCA", "clg": "SVVV"}
