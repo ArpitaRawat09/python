e1ed57a673612c9e1e57931c8265051811e36aae
Arpita Rawat <arpitarawat381@gmail.com>
2026-08-16 12:41:59 +0530

    frequency question

--- DS/dictonary.py
+++ DS/dictonary.py
@@ -1,4 +1,18 @@
-dictonary = {"name": "Arpita", "age ": 22, "degree": "MCA", "clg": "SVVV"}
+# Find the frequency
+
+x = [1, 1, 2, 3, 2, 4, 3, 5, 9, 9, 9, 2, 4, 1, 1, 0]
+d = {}
+freq = 0
+for i in x:
+    if i in d.keys():
+        d[i] += 1
+    else:
+        d[i] = 1    
+
+print(d)
+
+
+# dictonary = {"name": "Arpita", "age ": 22, "degree": "MCA", "clg": "SVVV"}
 # print(dictonary)
 # print(dictonary["name"])
 
@@ -6,8 +20,8 @@ dictonary = {"name": "Arpita", "age ": 22, "degree": "MCA", "clg": "SVVV"}
 #     print(dictonary[i])
 
 # dictonary.update({"city":"Indore"})
-x = dictonary.values()
-print(x)
+# x = dictonary.values()
+# print(x)
 
 
 # car = {
