527c6b6b035a5332a5b62e39fb253720d722f112
Arpita Rawat <arpitarawat381@gmail.com>
2026-08-16 13:01:20 +0530

    ZeroDivisionError

--- exceptionhandling.py
+++ exceptionhandling.py
@@ -1,11 +1,32 @@
+# Zero Division errror :- Exceptions
+
+a = int(input("enter number :- "))
+
+b = (10/a)
+print(b)
+
+
+# how to fix it 
+
+
+
+
+
 # syntax error
 
- x = [1,2,3,4,5]
+# x = [1,2,3,4,5]
 
 # for i in x:
 # print(i) 
 
 
 # indentation error
-for i in x
-    print(i)
+# for i in x
+#     print(i)
+
+
+
+
+
+
+
