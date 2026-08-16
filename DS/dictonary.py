a2725ebba796681aea1b3ef0daef9266b562bcb2
Arpita Rawat <arpitarawat381@gmail.com>
2026-08-16 12:34:58 +0530

    some methods of dictonary

--- DS/dictonary.py
+++ DS/dictonary.py
@@ -1,3 +1,22 @@
 dictonary = {"name": "Arpita", "age ": 22, "degree": "MCA", "clg": "SVVV"}
-print(dictonary)
-print(dictonary["name"])
+# print(dictonary)
+# print(dictonary["name"])
+
+# for i in dictonary:
+#     print(dictonary[i])
+
+# dictonary.update({"city":"Indore"})
+x = dictonary.values()
+print(x)
+
+
+# car = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+
+# x = car.setdefault("model", "Bronco")
+
+# print(x)
+# print(car)
