b324c1a2a6c48ef4b36dcf078978f90e5e18b2eb
Arpita Rawat <arpitarawat381@gmail.com>
2026-08-18 23:55:18 +0530

    class concept

---
+++ OOPS/class.py
@@ -0,0 +1,13 @@
+class Mca:
+    branch = "Computer Science"
+
+    def attendance(self):
+        print("All students are present in the class!")
+
+
+print("But today 1 lecture is free")
+
+
+# call class
+print(Mca().branch)
+Mca().attendance()
